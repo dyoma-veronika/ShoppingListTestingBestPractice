7fe0b1cfdc3cf4a55e6acb53784e7c46f7bcde0a
Veronika <dyoma.veronika@gmail.com>
2022-12-06 21:26:25 +0100

    architectural skeleton

--- app/src/main/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/di/AppModule.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/di/AppModule.kt
@@ -2,10 +2,13 @@ package com.androiddevs.shoppinglisttestingbestpracticeplackner.di
 
 import android.content.Context
 import androidx.room.Room
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.data.local.ShoppingDao
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.data.local.ShoppingItemDatabase
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.data.remote.PixabayAPI
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.other.Constants.BASE_URL
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.other.Constants.DATABASE_NAME
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.repositories.DefaultShoppingRepository
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.repositories.ShoppingRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -24,6 +27,13 @@ object AppModule {
     fun provideShoppingItemDatabase(
         @ApplicationContext context: Context
     ) = Room.databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideDefaultShoppingRepository(
+        dao: ShoppingDao,
+        api: PixabayAPI
+    ) = DefaultShoppingRepository(dao, api) as ShoppingRepository
 
     @Singleton
     @Provides
---
+++ app/src/main/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/ui/ShoppingViewModel.kt
@@ -0,0 +1,51 @@
+package com.androiddevs.shoppinglisttestingbestpracticeplackner.ui
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.other.Event
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.other.Resource
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.repositories.ShoppingRepository
+import kotlinx.coroutines.launch
+
+class ShoppingViewModel @ViewModelInject constructor(
+    private val repository: ShoppingRepository
+): ViewModel() {
+
+    val shoppingItems = repository.observeAllShoppingItems()
+
+    val totalPrice = repository.observeTotalPrice()
+
+    private val _images = MutableLiveData<Event<Resource<ImageResponse>>>()
+    val images: LiveData<Event<Resource<ImageResponse>>> = _images
+
+    private val _currentImageUrl = MutableLiveData<String>()
+    val currentImageUrl: LiveData<String> = _currentImageUrl
+
+    private val _insertShoppingItemStatus = MutableLiveData<Event<Resource<ShoppingItem>>>()
+    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> = _insertShoppingItemStatus
+
+    fun setCurrentImageUrl(url: String) {
+        _currentImageUrl.postValue(url)
+    }
+
+    fun deleteShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.deleteShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItemIntoDb(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.insertShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItem(name: String, amountString: String, priceString: String) {
+
+    }
+
+    fun searchForImage(imageQuery: String) {
+
+    }
+}
