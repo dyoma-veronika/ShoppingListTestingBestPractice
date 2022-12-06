81a8a813be016b83ebed4219f59738e3ca1e56d9
Veronika <dyoma.veronika@gmail.com>
2022-12-06 22:57:15 +0100

    add View Model Tests

--- app/src/main/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/ui/ShoppingViewModel.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/ui/ShoppingViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.data.local.ShoppingItem
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.other.Constants
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.other.Event
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.other.Resource
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.repositories.ShoppingRepository
@@ -14,7 +15,7 @@ import kotlinx.coroutines.launch
 
 class ShoppingViewModel @ViewModelInject constructor(
     private val repository: ShoppingRepository
-): ViewModel() {
+) : ViewModel() {
 
     val shoppingItems = repository.observeAllShoppingItems()
 
@@ -27,10 +28,12 @@ class ShoppingViewModel @ViewModelInject constructor(
     val currentImageUrl: LiveData<String> = _currentImageUrl
 
     private val _insertShoppingItemStatus = MutableLiveData<Event<Resource<ShoppingItem>>>()
-    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> = _insertShoppingItemStatus
+    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> =
+        _insertShoppingItemStatus
 
     fun setCurrentImageUrl(url: String) {
         _currentImageUrl.postValue(url)
+        // TODO: test if we call this fun with any url if this url can be observe from this live data
     }
 
     fun deleteShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launch {
@@ -42,10 +45,67 @@ class ShoppingViewModel @ViewModelInject constructor(
     }
 
     fun insertShoppingItem(name: String, amountString: String, priceString: String) {
-
+        if (name.isEmpty() || amountString.isEmpty() || priceString.isEmpty()) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The fields must not be empty",
+                        null
+                    )
+                )
+            )
+            return
+        }
+        if (name.length > Constants.MAX_NAME_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The name of the item" +
+                                "must not exceed ${Constants.MAX_NAME_LENGTH} characters", null
+                    )
+                )
+            )
+            return
+        }
+        if (priceString.length > Constants.MAX_PRICE_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "The price of the item" +
+                                "must not exceed ${Constants.MAX_PRICE_LENGTH} characters", null
+                    )
+                )
+            )
+            return
+        }
+        val amount = try {
+            amountString.toInt()
+        } catch (e: Exception) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        "Please enter valid amount",
+                        null
+                    )
+                )
+            )
+            return
+        }
+        val shoppingItem =
+            ShoppingItem(name, amount, priceString.toFloat(), _currentImageUrl.value ?: "")
+        insertShoppingItemIntoDb(shoppingItem)
+        setCurrentImageUrl("")// TODO: test if it's really sets current image url to empty string after we successfully inserted item
+        _insertShoppingItemStatus.postValue(Event(Resource.success(shoppingItem)))
     }
 
     fun searchForImage(imageQuery: String) {
-
+        if (imageQuery.isEmpty()) {
+            return
+        }
+        _images.value = Event(Resource.loading(null))
+        viewModelScope.launch {
+            val response = repository.searchForImage(imageQuery)
+            _images.value = Event(response)
+        }
     }
 }
