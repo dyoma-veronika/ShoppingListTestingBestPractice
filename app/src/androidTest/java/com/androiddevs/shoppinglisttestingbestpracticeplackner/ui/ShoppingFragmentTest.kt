0ecdcf36607a9968c71bca8a67d91457b3d105bd
Veronika <dyoma.veronika@gmail.com>
2022-12-12 14:13:19 +0100

    swipe delete item test

--- app/src/androidTest/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/ui/ShoppingFragmentTest.kt
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/ui/ShoppingFragmentTest.kt
@@ -1,9 +1,12 @@
 package com.androiddevs.shoppinglisttestingbestpracticeplackner.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.launchFragmentInHiltContainer
@@ -15,7 +18,12 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.R
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.adapters.ShoppingItemAdapter
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.getOrAwaitValue
+import com.google.common.truth.Truth.assertThat
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -23,11 +31,38 @@ import org.mockito.Mockito.verify
 class ShoppingFragmentTest {
 
     @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var testFragmentFactory: TestShoppingFragmentFactory
 
     @Before
     fun setup() {
         hiltRule.inject()
+    }
+
+    @Test
+    fun swipeShoppingItem_deleteItemInDb() {
+        val shoppingItem = ShoppingItem("Test", 1, 1f, "Test", 1)
+        var testViewModel: ShoppingViewModel? = null
+        launchFragmentInHiltContainer<ShoppingFragment>(
+            fragmentFactory = testFragmentFactory
+        ) {
+            testViewModel = viewModel
+            viewModel?.insertShoppingItemIntoDb(shoppingItem)
+        }
+
+        onView(withId(R.id.rvShoppingItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ShoppingItemAdapter.ShoppingItemViewHolder>(
+                0,
+                swipeLeft()
+            )
+        )
+
+        assertThat(testViewModel?.shoppingItems?.getOrAwaitValue()).isEmpty()
     }
 
     @Test
--- app/src/androidTest/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/ui/TestShoppingFragmentFactory.kt
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/ui/TestShoppingFragmentFactory.kt
@@ -4,10 +4,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.adapters.ImageAdapter
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.adapters.ShoppingItemAdapter
+import com.androiddevs.shoppinglisttestingbestpracticeplackner.repositories.FakeShoppingRepositoryAndroidTest
 import com.bumptech.glide.RequestManager
 import javax.inject.Inject
 
-class ShoppingFragmentFactory @Inject constructor(
+class TestShoppingFragmentFactory @Inject constructor(
     private val imageAdapter: ImageAdapter,
     private val glide: RequestManager,
     private val shoppingItemAdapter: ShoppingItemAdapter
@@ -16,7 +17,10 @@ class ShoppingFragmentFactory @Inject constructor(
         return when (className) {
             ImagePickFragment::class.java.name -> ImagePickFragment(imageAdapter)
             AddShoppingItemFragment::class.java.name -> AddShoppingItemFragment(glide = glide)
-            ShoppingFragment::class.java.name -> ShoppingFragment(shoppingItemAdapter)
+            ShoppingFragment::class.java.name -> ShoppingFragment(
+                shoppingItemAdapter,
+                ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+            )
             else -> super.instantiate(classLoader, className)
         }
     }
