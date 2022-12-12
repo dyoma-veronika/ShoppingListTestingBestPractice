3fb1c28d359013e4abf1910dae9358aabc5fa1f1
Veronika <dyoma.veronika@gmail.com>
2022-12-12 18:35:34 +0100

    add hilt to Main Activity

--- app/src/main/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingbestpracticeplackner/ui/MainActivity.kt
@@ -3,11 +3,18 @@ package com.androiddevs.shoppinglisttestingbestpracticeplackner.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.androiddevs.shoppinglisttestingbestpracticeplackner.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.fragmentFactory = fragmentFactory
         setContentView(R.layout.activity_main)
     }
 }
